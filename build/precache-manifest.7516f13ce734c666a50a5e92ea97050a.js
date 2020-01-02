9518b8b78332c3e5462792dc31c795b1f47f7aa8
Jain <anshujai@88e9fe6d2bfa.ant.amazon.com>
2020-01-02 16:51:46 +0530

    cdc

---
+++ build/precache-manifest.7516f13ce734c666a50a5e92ea97050a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d575cb9e1f1dcfdb8d38cd8aa37c2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f4a23558cbda935349e",
+    "url": "/static/css/2.65b39531.chunk.css"
+  },
+  {
+    "revision": "021267be1022992f95a4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0f4a23558cbda935349e",
+    "url": "/static/js/2.83b51282.chunk.js"
+  },
+  {
+    "revision": "0243f6fd1e8a300c53eafce5405c216e",
+    "url": "/static/js/2.83b51282.chunk.js.LICENSE"
+  },
+  {
+    "revision": "021267be1022992f95a4",
+    "url": "/static/js/main.04cbb90b.chunk.js"
+  },
+  {
+    "revision": "ab6748f566b9656776f1",
+    "url": "/static/js/runtime-main.36ef7111.js"
+  }
+]);
